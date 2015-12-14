ebeff1fbd28900ccad154efcd154d95651e518ef
Eddy Lane <naedin@gmail.com>
2015-12-14 23:26:01 +0000

    Some json parsing

--- elm.js
+++ elm.js
@@ -3742,6 +3742,116 @@ Elm.Native.Signal.make = function(localRuntime) {
 	};
 };
 
+Elm.Native.Time = {};
+
+Elm.Native.Time.make = function(localRuntime)
+{
+	localRuntime.Native = localRuntime.Native || {};
+	localRuntime.Native.Time = localRuntime.Native.Time || {};
+	if (localRuntime.Native.Time.values)
+	{
+		return localRuntime.Native.Time.values;
+	}
+
+	var NS = Elm.Native.Signal.make(localRuntime);
+	var Maybe = Elm.Maybe.make(localRuntime);
+
+
+	// FRAMES PER SECOND
+
+	function fpsWhen(desiredFPS, isOn)
+	{
+		var msPerFrame = 1000 / desiredFPS;
+		var ticker = NS.input('fps-' + desiredFPS, null);
+
+		function notifyTicker()
+		{
+			localRuntime.notify(ticker.id, null);
+		}
+
+		function firstArg(x, y)
+		{
+			return x;
+		}
+
+		// input fires either when isOn changes, or when ticker fires.
+		// Its value is a tuple with the current timestamp, and the state of isOn
+		var input = NS.timestamp(A3(NS.map2, F2(firstArg), NS.dropRepeats(isOn), ticker));
+
+		var initialState = {
+			isOn: false,
+			time: localRuntime.timer.programStart,
+			delta: 0
+		};
+
+		var timeoutId;
+
+		function update(input, state)
+		{
+			var currentTime = input._0;
+			var isOn = input._1;
+			var wasOn = state.isOn;
+			var previousTime = state.time;
+
+			if (isOn)
+			{
+				timeoutId = localRuntime.setTimeout(notifyTicker, msPerFrame);
+			}
+			else if (wasOn)
+			{
+				clearTimeout(timeoutId);
+			}
+
+			return {
+				isOn: isOn,
+				time: currentTime,
+				delta: (isOn && !wasOn) ? 0 : currentTime - previousTime
+			};
+		}
+
+		return A2(
+			NS.map,
+			function(state) { return state.delta; },
+			A3(NS.foldp, F2(update), update(input.value, initialState), input)
+		);
+	}
+
+
+	// EVERY
+
+	function every(t)
+	{
+		var ticker = NS.input('every-' + t, null);
+		function tellTime()
+		{
+			localRuntime.notify(ticker.id, null);
+		}
+		var clock = A2(NS.map, fst, NS.timestamp(ticker));
+		setInterval(tellTime, t);
+		return clock;
+	}
+
+
+	function fst(pair)
+	{
+		return pair._0;
+	}
+
+
+	function read(s)
+	{
+		var t = Date.parse(s);
+		return isNaN(t) ? Maybe.Nothing : Maybe.Just(t);
+	}
+
+	return localRuntime.Native.Time.values = {
+		fpsWhen: F2(fpsWhen),
+		every: every,
+		toDate: function(t) { return new Date(t); },
+		read: read
+	};
+};
+
 Elm.Native.Transform2D = {};
 Elm.Native.Transform2D.make = function(localRuntime) {
 	localRuntime.Native = localRuntime.Native || {};
@@ -6516,6 +6626,52 @@ Elm.Signal.make = function (_elm) {
                                ,message: message
                                ,forwardTo: forwardTo
                                ,Mailbox: Mailbox};
+};
+Elm.Time = Elm.Time || {};
+Elm.Time.make = function (_elm) {
+   "use strict";
+   _elm.Time = _elm.Time || {};
+   if (_elm.Time.values) return _elm.Time.values;
+   var _U = Elm.Native.Utils.make(_elm),
+   $Basics = Elm.Basics.make(_elm),
+   $Native$Signal = Elm.Native.Signal.make(_elm),
+   $Native$Time = Elm.Native.Time.make(_elm),
+   $Signal = Elm.Signal.make(_elm);
+   var _op = {};
+   var delay = $Native$Signal.delay;
+   var since = F2(function (time,signal) {
+      var stop = A2($Signal.map,$Basics.always(-1),A2(delay,time,signal));
+      var start = A2($Signal.map,$Basics.always(1),signal);
+      var delaydiff = A3($Signal.foldp,F2(function (x,y) {    return x + y;}),0,A2($Signal.merge,start,stop));
+      return A2($Signal.map,F2(function (x,y) {    return !_U.eq(x,y);})(0),delaydiff);
+   });
+   var timestamp = $Native$Signal.timestamp;
+   var every = $Native$Time.every;
+   var fpsWhen = $Native$Time.fpsWhen;
+   var fps = function (targetFrames) {    return A2(fpsWhen,targetFrames,$Signal.constant(true));};
+   var inMilliseconds = function (t) {    return t;};
+   var millisecond = 1;
+   var second = 1000 * millisecond;
+   var minute = 60 * second;
+   var hour = 60 * minute;
+   var inHours = function (t) {    return t / hour;};
+   var inMinutes = function (t) {    return t / minute;};
+   var inSeconds = function (t) {    return t / second;};
+   return _elm.Time.values = {_op: _op
+                             ,millisecond: millisecond
+                             ,second: second
+                             ,minute: minute
+                             ,hour: hour
+                             ,inMilliseconds: inMilliseconds
+                             ,inSeconds: inSeconds
+                             ,inMinutes: inMinutes
+                             ,inHours: inHours
+                             ,fps: fps
+                             ,fpsWhen: fpsWhen
+                             ,every: every
+                             ,timestamp: timestamp
+                             ,delay: delay
+                             ,since: since};
 };
 Elm.Native.String = {};
 
@@ -10286,6 +10442,310 @@ Elm.Html.Events.make = function (_elm) {
                                     ,keyCode: keyCode
                                     ,Options: Options};
 };
+Elm.Native.Http = {};
+Elm.Native.Http.make = function(localRuntime) {
+
+	localRuntime.Native = localRuntime.Native || {};
+	localRuntime.Native.Http = localRuntime.Native.Http || {};
+	if (localRuntime.Native.Http.values)
+	{
+		return localRuntime.Native.Http.values;
+	}
+
+	var Dict = Elm.Dict.make(localRuntime);
+	var List = Elm.List.make(localRuntime);
+	var Maybe = Elm.Maybe.make(localRuntime);
+	var Task = Elm.Native.Task.make(localRuntime);
+
+
+	function send(settings, request)
+	{
+		return Task.asyncFunction(function(callback) {
+			var req = new XMLHttpRequest();
+
+			// start
+			if (settings.onStart.ctor === 'Just')
+			{
+				req.addEventListener('loadStart', function() {
+					var task = settings.onStart._0;
+					Task.spawn(task);
+				});
+			}
+
+			// progress
+			if (settings.onProgress.ctor === 'Just')
+			{
+				req.addEventListener('progress', function(event) {
+					var progress = !event.lengthComputable
+						? Maybe.Nothing
+						: Maybe.Just({
+							_: {},
+							loaded: event.loaded,
+							total: event.total
+						});
+					var task = settings.onProgress._0(progress);
+					Task.spawn(task);
+				});
+			}
+
+			// end
+			req.addEventListener('error', function() {
+				return callback(Task.fail({ ctor: 'RawNetworkError' }));
+			});
+
+			req.addEventListener('timeout', function() {
+				return callback(Task.fail({ ctor: 'RawTimeout' }));
+			});
+
+			req.addEventListener('load', function() {
+				return callback(Task.succeed(toResponse(req)));
+			});
+
+			req.open(request.verb, request.url, true);
+
+			// set all the headers
+			function setHeader(pair) {
+				req.setRequestHeader(pair._0, pair._1);
+			}
+			A2(List.map, setHeader, request.headers);
+
+			// set the timeout
+			req.timeout = settings.timeout;
+
+			// enable this withCredentials thing
+			req.withCredentials = settings.withCredentials;
+
+			// ask for a specific MIME type for the response
+			if (settings.desiredResponseType.ctor === 'Just')
+			{
+				req.overrideMimeType(settings.desiredResponseType._0);
+			}
+
+			// actuall send the request
+			if(request.body.ctor === "BodyFormData")
+			{
+				req.send(request.body.formData)
+			}
+			else
+			{
+				req.send(request.body._0);
+			}
+		});
+	}
+
+
+	// deal with responses
+
+	function toResponse(req)
+	{
+		var tag = req.responseType === 'blob' ? 'Blob' : 'Text'
+		var response = tag === 'Blob' ? req.response : req.responseText;
+		return {
+			_: {},
+			status: req.status,
+			statusText: req.statusText,
+			headers: parseHeaders(req.getAllResponseHeaders()),
+			url: req.responseURL,
+			value: { ctor: tag, _0: response }
+		};
+	}
+
+
+	function parseHeaders(rawHeaders)
+	{
+		var headers = Dict.empty;
+
+		if (!rawHeaders)
+		{
+			return headers;
+		}
+
+		var headerPairs = rawHeaders.split('\u000d\u000a');
+		for (var i = headerPairs.length; i--; )
+		{
+			var headerPair = headerPairs[i];
+			var index = headerPair.indexOf('\u003a\u0020');
+			if (index > 0)
+			{
+				var key = headerPair.substring(0, index);
+				var value = headerPair.substring(index + 2);
+
+				headers = A3(Dict.update, key, function(oldValue) {
+					if (oldValue.ctor === 'Just')
+					{
+						return Maybe.Just(value + ', ' + oldValue._0);
+					}
+					return Maybe.Just(value);
+				}, headers);
+			}
+		}
+
+		return headers;
+	}
+
+
+	function multipart(dataList)
+	{
+		var formData = new FormData();
+
+		while (dataList.ctor !== '[]')
+		{
+			var data = dataList._0;
+			if (data.ctor === 'StringData')
+			{
+				formData.append(data._0, data._1);
+			}
+			else
+			{
+				var fileName = data._1.ctor === 'Nothing'
+					? undefined
+					: data._1._0;
+				formData.append(data._0, data._2, fileName);
+			}
+			dataList = dataList._1;
+		}
+
+		return { ctor: 'BodyFormData', formData: formData };
+	}
+
+
+	function uriEncode(string)
+	{
+		return encodeURIComponent(string);
+	}
+
+	function uriDecode(string)
+	{
+		return decodeURIComponent(string);
+	}
+
+	return localRuntime.Native.Http.values = {
+		send: F2(send),
+		multipart: multipart,
+		uriEncode: uriEncode,
+		uriDecode: uriDecode
+	};
+};
+
+Elm.Http = Elm.Http || {};
+Elm.Http.make = function (_elm) {
+   "use strict";
+   _elm.Http = _elm.Http || {};
+   if (_elm.Http.values) return _elm.Http.values;
+   var _U = Elm.Native.Utils.make(_elm),
+   $Basics = Elm.Basics.make(_elm),
+   $Debug = Elm.Debug.make(_elm),
+   $Dict = Elm.Dict.make(_elm),
+   $Json$Decode = Elm.Json.Decode.make(_elm),
+   $List = Elm.List.make(_elm),
+   $Maybe = Elm.Maybe.make(_elm),
+   $Native$Http = Elm.Native.Http.make(_elm),
+   $Result = Elm.Result.make(_elm),
+   $Signal = Elm.Signal.make(_elm),
+   $String = Elm.String.make(_elm),
+   $Task = Elm.Task.make(_elm),
+   $Time = Elm.Time.make(_elm);
+   var _op = {};
+   var send = $Native$Http.send;
+   var BadResponse = F2(function (a,b) {    return {ctor: "BadResponse",_0: a,_1: b};});
+   var UnexpectedPayload = function (a) {    return {ctor: "UnexpectedPayload",_0: a};};
+   var handleResponse = F2(function (handle,response) {
+      if (_U.cmp(200,response.status) < 1 && _U.cmp(response.status,300) < 0) {
+            var _p0 = response.value;
+            if (_p0.ctor === "Text") {
+                  return handle(_p0._0);
+               } else {
+                  return $Task.fail(UnexpectedPayload("Response body is a blob, expecting a string."));
+               }
+         } else return $Task.fail(A2(BadResponse,response.status,response.statusText));
+   });
+   var NetworkError = {ctor: "NetworkError"};
+   var Timeout = {ctor: "Timeout"};
+   var promoteError = function (rawError) {    var _p1 = rawError;if (_p1.ctor === "RawTimeout") {    return Timeout;} else {    return NetworkError;}};
+   var fromJson = F2(function (decoder,response) {
+      var decode = function (str) {
+         var _p2 = A2($Json$Decode.decodeString,decoder,str);
+         if (_p2.ctor === "Ok") {
+               return $Task.succeed(_p2._0);
+            } else {
+               return $Task.fail(UnexpectedPayload(_p2._0));
+            }
+      };
+      return A2($Task.andThen,A2($Task.mapError,promoteError,response),handleResponse(decode));
+   });
+   var RawNetworkError = {ctor: "RawNetworkError"};
+   var RawTimeout = {ctor: "RawTimeout"};
+   var Blob = function (a) {    return {ctor: "Blob",_0: a};};
+   var Text = function (a) {    return {ctor: "Text",_0: a};};
+   var Response = F5(function (a,b,c,d,e) {    return {status: a,statusText: b,headers: c,url: d,value: e};});
+   var defaultSettings = {timeout: 0,onStart: $Maybe.Nothing,onProgress: $Maybe.Nothing,desiredResponseType: $Maybe.Nothing,withCredentials: false};
+   var post = F3(function (decoder,url,body) {
+      var request = {verb: "POST",headers: _U.list([]),url: url,body: body};
+      return A2(fromJson,decoder,A2(send,defaultSettings,request));
+   });
+   var Settings = F5(function (a,b,c,d,e) {    return {timeout: a,onStart: b,onProgress: c,desiredResponseType: d,withCredentials: e};});
+   var multipart = $Native$Http.multipart;
+   var FileData = F3(function (a,b,c) {    return {ctor: "FileData",_0: a,_1: b,_2: c};});
+   var BlobData = F3(function (a,b,c) {    return {ctor: "BlobData",_0: a,_1: b,_2: c};});
+   var blobData = BlobData;
+   var StringData = F2(function (a,b) {    return {ctor: "StringData",_0: a,_1: b};});
+   var stringData = StringData;
+   var BodyBlob = function (a) {    return {ctor: "BodyBlob",_0: a};};
+   var BodyFormData = {ctor: "BodyFormData"};
+   var ArrayBuffer = {ctor: "ArrayBuffer"};
+   var BodyString = function (a) {    return {ctor: "BodyString",_0: a};};
+   var string = BodyString;
+   var Empty = {ctor: "Empty"};
+   var empty = Empty;
+   var getString = function (url) {
+      var request = {verb: "GET",headers: _U.list([]),url: url,body: empty};
+      return A2($Task.andThen,A2($Task.mapError,promoteError,A2(send,defaultSettings,request)),handleResponse($Task.succeed));
+   };
+   var get = F2(function (decoder,url) {
+      var request = {verb: "GET",headers: _U.list([]),url: url,body: empty};
+      return A2(fromJson,decoder,A2(send,defaultSettings,request));
+   });
+   var Request = F4(function (a,b,c,d) {    return {verb: a,headers: b,url: c,body: d};});
+   var uriDecode = $Native$Http.uriDecode;
+   var uriEncode = $Native$Http.uriEncode;
+   var queryEscape = function (string) {    return A2($String.join,"+",A2($String.split,"%20",uriEncode(string)));};
+   var queryPair = function (_p3) {    var _p4 = _p3;return A2($Basics._op["++"],queryEscape(_p4._0),A2($Basics._op["++"],"=",queryEscape(_p4._1)));};
+   var url = F2(function (baseUrl,args) {
+      var _p5 = args;
+      if (_p5.ctor === "[]") {
+            return baseUrl;
+         } else {
+            return A2($Basics._op["++"],baseUrl,A2($Basics._op["++"],"?",A2($String.join,"&",A2($List.map,queryPair,args))));
+         }
+   });
+   var TODO_implement_file_in_another_library = {ctor: "TODO_implement_file_in_another_library"};
+   var TODO_implement_blob_in_another_library = {ctor: "TODO_implement_blob_in_another_library"};
+   return _elm.Http.values = {_op: _op
+                             ,getString: getString
+                             ,get: get
+                             ,post: post
+                             ,send: send
+                             ,url: url
+                             ,uriEncode: uriEncode
+                             ,uriDecode: uriDecode
+                             ,empty: empty
+                             ,string: string
+                             ,multipart: multipart
+                             ,stringData: stringData
+                             ,defaultSettings: defaultSettings
+                             ,fromJson: fromJson
+                             ,Request: Request
+                             ,Settings: Settings
+                             ,Response: Response
+                             ,Text: Text
+                             ,Blob: Blob
+                             ,Timeout: Timeout
+                             ,NetworkError: NetworkError
+                             ,UnexpectedPayload: UnexpectedPayload
+                             ,BadResponse: BadResponse
+                             ,RawTimeout: RawTimeout
+                             ,RawNetworkError: RawNetworkError};
+};
 Elm.StartApp = Elm.StartApp || {};
 Elm.StartApp.Simple = Elm.StartApp.Simple || {};
 Elm.StartApp.Simple.make = function (_elm) {
@@ -10330,6 +10790,7 @@ Elm.Cloudcatcher.make = function (_elm) {
    $Html = Elm.Html.make(_elm),
    $Html$Attributes = Elm.Html.Attributes.make(_elm),
    $Html$Events = Elm.Html.Events.make(_elm),
+   $Json$Decode = Elm.Json.Decode.make(_elm),
    $List = Elm.List.make(_elm),
    $Maybe = Elm.Maybe.make(_elm),
    $Result = Elm.Result.make(_elm),
@@ -10337,6 +10798,11 @@ Elm.Cloudcatcher.make = function (_elm) {
    $StartApp$Simple = Elm.StartApp.Simple.make(_elm),
    $String = Elm.String.make(_elm);
    var _op = {};
+   var podcastListItem = F2(function (address,podcast) {    return A2($Html.li,_U.list([]),_U.list([$Html.text(podcast.collectionName)]));});
+   var podcastList = F2(function (address,entries) {
+      var entryItems = A2($List.map,podcastListItem(address),entries);
+      return A2($Html.ul,_U.list([]),entryItems);
+   });
    var pageFooter = A2($Html.footer,
    _U.list([]),
    _U.list([A2($Html.a,_U.list([$Html$Attributes.href("https://google.com")]),_U.list([$Html.text("Cloudcatcher")]))]));
@@ -10351,8 +10817,15 @@ Elm.Cloudcatcher.make = function (_elm) {
    });
    var UpdateSearchInput = function (a) {    return {ctor: "UpdateSearchInput",_0: a};};
    var NoOp = {ctor: "NoOp"};
-   var initialModel = {searchInput: ""};
-   var Model = function (a) {    return {searchInput: a};};
+   var initialModel = {entries: _U.list([]),searchInput: ""};
+   var Podcast = function (a) {    return {collectionName: a};};
+   var Model = F2(function (a,b) {    return {entries: a,searchInput: b};});
+   var newPodcast = function (name) {    return {collectionName: name};};
+   var decoder = A2($Json$Decode.object1,Podcast,A2($Json$Decode._op[":="],"collectionName",$Json$Decode.string));
+   var decoderCol = A2($Json$Decode.object1,$Basics.identity,A2($Json$Decode._op[":="],"results",$Json$Decode.list(decoder)));
+   var testData = "{\"results\": [{ \"collectionName\": \"Test Podcast\" }, { \"collectionName\": \"Test Podcast Two\" }]}";
+   var decodedVals = A2($Json$Decode.decodeString,decoderCol,testData);
+   var log = A2($Debug.log,"decodedVals",decodedVals);
    var parseInt = function (string) {    var _p1 = $String.toInt(string);if (_p1.ctor === "Ok") {    return _p1._0;} else {    return 0;}};
    var onInput = F2(function (address,f) {
       return A3($Html$Events.on,"input",$Html$Events.targetValue,function (v) {    return A2($Signal.message,address,f(v));});
@@ -10372,20 +10845,31 @@ Elm.Cloudcatcher.make = function (_elm) {
               ,A2($Html.button,_U.list([$Html$Attributes.$class("btn btn-primary")]),_U.list([$Html.text("Search")]))]));
    });
    var view = F2(function (address,model) {
-      return A2($Html.div,_U.list([$Html$Attributes.$class("container")]),_U.list([pageHeader,A2(searchForm,address,model),pageFooter]));
+      return A2($Html.div,
+      _U.list([$Html$Attributes.$class("container")]),
+      _U.list([pageHeader,A2(searchForm,address,model),A2(podcastList,address,model.entries),pageFooter]));
    });
    var main = $StartApp$Simple.start({model: initialModel,view: view,update: update});
    return _elm.Cloudcatcher.values = {_op: _op
                                      ,onInput: onInput
                                      ,parseInt: parseInt
+                                     ,testData: testData
+                                     ,decoder: decoder
+                                     ,decoderCol: decoderCol
+                                     ,newPodcast: newPodcast
                                      ,Model: Model
+                                     ,Podcast: Podcast
+                                     ,decodedVals: decodedVals
                                      ,initialModel: initialModel
                                      ,NoOp: NoOp
                                      ,UpdateSearchInput: UpdateSearchInput
                                      ,update: update
+                                     ,log: log
                                      ,pageHeader: pageHeader
                                      ,pageFooter: pageFooter
                                      ,searchForm: searchForm
+                                     ,podcastListItem: podcastListItem
+                                     ,podcastList: podcastList
                                      ,view: view
                                      ,main: main};
 };
--- src/Cloudcatcher.elm
+++ src/Cloudcatcher.elm
@@ -8,6 +8,8 @@ import String exposing (toInt)
 import Http
 import StartApp.Simple as StartApp
 import Json.Decode as Decode exposing (Decoder, (:=))
+import Debug
+import Result exposing (..)
 
 -- UTILS
 
@@ -26,39 +28,52 @@ parseInt string =
 --searchForPodcast query =
 --  Http.get "https://itunes.apple.com/search?term=" ++ query ++ "&media=podcast"
 
-testData = "{\"resultCount\":1,\"results\": [{\"wrapperType\":\"track\", \"kind\":\"podcast\", \"artistId\":825701899, \"collectionId\":496893300, \"trackId\":496893300, \"artistName\":\"DevChat.tv\", \"collectionName\":\"JavaScript Jabber\", \"trackName\":\"JavaScript Jabber\", \"collectionCensoredName\":\"JavaScript Jabber\", \"trackCensoredName\":\"JavaScript Jabber\", \"artistViewUrl\":\"https://itunes.apple.com/us/artist/devchat.tv/id825701899?mt=2&uo=4\", \"collectionViewUrl\":\"https://itunes.apple.com/us/podcast/javascript-jabber/id496893300?mt=2&uo=4\", \"feedUrl\":\"http://feeds.feedwrench.com/JavaScriptJabber.rss\", \"trackViewUrl\":\"https://itunes.apple.com/us/podcast/javascript-jabber/id496893300?mt=2&uo=4\", \"artworkUrl30\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/30x30bb.jpg\", \"artworkUrl60\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/60x60bb.jpg\", \"artworkUrl100\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/100x100bb.jpg\", \"collectionPrice\":0.00, \"trackPrice\":0.00, \"trackRentalPrice\":0, \"collectionHdPrice\":0, \"trackHdPrice\":0, \"trackHdRentalPrice\":0, \"releaseDate\":\"2015-12-09T16:00:00Z\", \"collectionExplicitness\":\"notExplicit\", \"trackExplicitness\":\"notExplicit\", \"trackCount\":189, \"country\":\"USA\", \"currency\":\"USD\", \"primaryGenreName\":\"Training\", \"radioStationUrl\":\"https://itunes.apple.com/station/idra.496893300\", \"artworkUrl600\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/600x600bb.jpg\", \"genreIds\":[\"1470\", \"26\", \"1304\", \"1318\", \"1480\"], \"genres\":[\"Training\", \"Podcasts\", \"Education\", \"Technology\", \"Software How-To\"]}]}"
 
-decoder : Decoder Podcast
-decoder = Decode.object1 Podcast
+--decoder : Decoder Podcast
+--decoder = Decode.object1 Podcast
+--            ("collectionName" := Decode.string )
+
+--decoderCol : Decoder (List (Podcast))
+--decoderCol = Decode.object1 identity
+--            ("results" := Decode.list decoder)
+
+
+podcasts : Decoder (List Podcast)
+podcasts =
+  let podcast =
+    Decode.object2 Podcast
             ("collectionName" := Decode.string )
+            ("artworkUrl100" := Decode.string )
+  in 
+    "results" := Decode.list podcast
 
-decoderCol : Decoder Collection
-decoderCol = Decode.object1 identity
-            ("results" := Decode.list decoder)
-
-
-newPodcast : String -> Podcast
-newPodcast name = 
+newPodcast : String -> String -> Podcast
+newPodcast name imageUrl = 
   {
-    collectionName = name
+    collectionName = name, 
+    imgUrl = imageUrl
   }
 
 -- MODEL
 
+type alias Collection = List Podcast
+
 type alias Model = 
-    { entries : Collection,
+    { entries : List Podcast,
       searchInput : String 
     }
 
 type alias Podcast =
-    { collectionName : String }
+    { collectionName : String,
+      imgUrl : String }
 
-type alias Collection = List Podcast
-
+--testData = "{\"results\": [{ \"collectionName\": \"Test Podcast\" }, { \"collectionName\": \"Test Podcast Two\" }]}"
+testData = "{\"resultCount\":1,\"results\": [{\"wrapperType\":\"track\", \"kind\":\"podcast\", \"artistId\":825701899, \"collectionId\":496893300, \"trackId\":496893300, \"artistName\":\"DevChat.tv\", \"collectionName\":\"JavaScript Jabber\", \"trackName\":\"JavaScript Jabber\", \"collectionCensoredName\":\"JavaScript Jabber\", \"trackCensoredName\":\"JavaScript Jabber\", \"artistViewUrl\":\"https://itunes.apple.com/us/artist/devchat.tv/id825701899?mt=2&uo=4\", \"collectionViewUrl\":\"https://itunes.apple.com/us/podcast/javascript-jabber/id496893300?mt=2&uo=4\", \"feedUrl\":\"http://feeds.feedwrench.com/JavaScriptJabber.rss\", \"trackViewUrl\":\"https://itunes.apple.com/us/podcast/javascript-jabber/id496893300?mt=2&uo=4\", \"artworkUrl30\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/30x30bb.jpg\", \"artworkUrl60\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/60x60bb.jpg\", \"artworkUrl100\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/100x100bb.jpg\", \"collectionPrice\":0.00, \"trackPrice\":0.00, \"trackRentalPrice\":0, \"collectionHdPrice\":0, \"trackHdPrice\":0, \"trackHdRentalPrice\":0, \"releaseDate\":\"2015-12-09T16:00:00Z\", \"collectionExplicitness\":\"notExplicit\", \"trackExplicitness\":\"notExplicit\", \"trackCount\":189, \"country\":\"USA\", \"currency\":\"USD\", \"primaryGenreName\":\"Training\", \"radioStationUrl\":\"https://itunes.apple.com/station/idra.496893300\", \"artworkUrl600\":\"http://is5.mzstatic.com/image/thumb/Music2/v4/7c/50/e9/7c50e99f-5f2a-9f41-1eaa-116fcee0eb68/source/600x600bb.jpg\", \"genreIds\":[\"1470\", \"26\", \"1304\", \"1318\", \"1480\"], \"genres\":[\"Training\", \"Podcasts\", \"Education\", \"Technology\", \"Software How-To\"]}]}"
 
 initialModel : Model
 initialModel = 
-    { entries = Decode.decodeString decoderCol testData,
+    { 
+      entries = withDefault [] ( Decode.decodeString podcasts testData ),
       searchInput = "" 
     }
 
@@ -68,6 +83,7 @@ initialModel =
 type Action
     = NoOp
     | UpdateSearchInput String
+    | SetResults Collection
 
 update : Action -> Model -> Model
 update action model =
@@ -77,6 +93,9 @@ update action model =
 
         UpdateSearchInput content ->
             { model | searchInput = content }
+
+        SetResults results ->
+            { model | entries = results }
 
 
 -- VIEW
@@ -110,7 +129,12 @@ searchForm address model =
 podcastListItem : Address Action -> Podcast -> Html
 podcastListItem address podcast = 
   li [ ] 
-     [ text podcast.collectionName ]
+     [ img [ src podcast.imgUrl ] [ ], 
+       text podcast.collectionName 
+     ]
+
+
+
 
 podcastList: Address Action -> List Podcast -> Html
 podcastList address entries = 
@@ -118,6 +142,8 @@ podcastList address entries =
     entryItems = List.map (podcastListItem address) entries
   in
     ul [ ] entryItems
+
+
 
 view : Address Action -> Model -> Html
 view address model = 
@@ -129,8 +155,6 @@ view address model =
       pageFooter
     ]
 
--- WIRE TEH APP TOGETHER!
-
 main: Signal Html
 main =
   StartApp.start
@@ -138,3 +162,4 @@ main =
       view = view,
      update = update
     }
+
